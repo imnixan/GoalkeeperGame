4bfcecb5decb51b3791a6f492fcdb7e7a856cf51
imnixan <->
2023-07-14 17:21:14 +0700

    finish

---
+++ Assets/Code/Global/GameGlobal/GameStepsMachine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameStepsMachine : MonoBehaviour
+{
+    private GameSteps _currentStep;
+
+    public GameSteps CurrentStep
+    {
+        get { return _currentStep; }
+        set
+        {
+            if (_currentStep != value)
+            {
+                _currentStep = value;
+            }
+        }
+    }
+
+    public enum GameSteps
+    {
+        Null,
+        WaitingNextButton,
+        Preparing,
+        Kick,
+        Goal,
+        Pause
+    }
+}
